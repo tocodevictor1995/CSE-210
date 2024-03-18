bffabf852192dcc8b98a9df8c8b713e92679480d
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-03-18 21:58:30 +0000

    Update Program.cs
    ok

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Assignment assignment = new("7.9","9-16");
-        assignment.SetStudentName("Godwin Akpan Essien");
+        assignment.SetStudentName("Victor Cordeiro Gomes");
         assignment.SetTopic("Fractions");
-        MathAssignment math = new("Godwin Essien","Fractions", "7.9","9-16");
+        MathAssignment math = new("Victor Cordeiro","Fractions", "7.9","9-16");
         // math.SetSection("7.3");
         // math.SetProblem("8-19");
         Console.WriteLine(assignment.GetSummary());
