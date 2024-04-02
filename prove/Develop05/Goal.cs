455a149f8c25710ce649b505d11d012fee1a1371
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-04-02 22:16:25 +0100

    Add files via upload

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,31 @@
+public abstract class Goal
+{
+    protected bool _isCompleted;
+    protected string _name;
+    protected string _description;
+    protected int _points;
+    protected int _type;
+
+    public Goal(string name, string description, int points, bool isCompleted)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _isCompleted = isCompleted;
+    }
+
+    public abstract void SetIsCompleted();
+    public abstract void DisplayGoal(int option);
+    public abstract string GetStringRep();
+
+
+    public bool GetIsCompleted()
+    {
+        return _isCompleted;
+    }
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+}
